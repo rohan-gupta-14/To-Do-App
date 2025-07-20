99e60a6282aeb0d3c0b26635afe05991fe01f618
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-07-20 14:15:29 +0530

    Hook and add function

--- OneDrive/Projects/To-do App/To-do/src/App.jsx
+++ OneDrive/Projects/To-do App/To-do/src/App.jsx
@@ -4,7 +4,29 @@ import viteLogo from '/vite.svg'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState('');
+  const [description, setDescription] = useState('');
+  const [todos, setTodos] = useState([]);
+  const handleAdd = () => {
+  
+  if (task.trim() === '') return;
+
+  
+  const newTask = {
+    id: Date.now(),         
+    task: task,             
+    description: description, 
+    completed: false        
+  };
+
+  
+  setTodos([...todos, newTask]);
+
+  
+  setTask('');
+  setDescription('');
+};
+
 
   return (
     <>
@@ -14,28 +36,30 @@ function App() {
         </div>
         <div className="max-w-2xl mx-auto p-6 bg-gradient-to-br from-white/90 to-amber-50 rounded-2xl shadow-lg backdrop-blur-sm">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            
+
             <div className="flex flex-col">
               <label className="text-lg font-semibold text-gray-700 mb-2">Task:</label>
               <input
                 type="text"
-                placeholder="Enter a Task"
-                className="border border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-200 rounded-lg px-4 py-3 transition"
+                placeholder="Enter a task"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
               />
-              <button className="mt-3 bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 transition">
+              <button onClick={handleAdd} className="mt-3 bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 transition">
                 Add Task
               </button>
             </div>
 
-            
+
             <div className="flex flex-col">
               <label className="text-lg font-semibold text-gray-700 mb-2">Description:</label>
               <input
                 type="text"
-                placeholder="Write the Description"
-                className="border border-gray-300 focus:border-green-400 focus:ring focus:ring-green-200 rounded-lg px-4 py-3 transition"
+                placeholder="Write the description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
-              <button className="mt-3 bg-green-500 text-white font-semibold py-2 rounded-lg hover:bg-green-600 focus:ring-4 focus:ring-green-300 transition">
+              <button onClick={handleAdd} className="mt-3 bg-green-500 text-white font-semibold py-2 rounded-lg hover:bg-green-600 focus:ring-4 focus:ring-green-300 transition">
                 Add Description
               </button>
             </div>
